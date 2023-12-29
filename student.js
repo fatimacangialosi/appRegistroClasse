3961e070d195b163462c330ce967a1195449457e
BarcaGiuseppe <131388369+BarcaGiuseppe@users.noreply.github.com>
2023-12-29 12:13:01 +0100

    feat(controller): add controller.js and define home logic (#11)
    Co-authored-by: Giuseppe Barca <giuseppe.barca@stevejobs.academy>

--- student.js
+++ student.js
@@ -12,37 +12,37 @@
 let studentList = [];
 
 function createStudent(iname, lastName, email, lessonId) {
-	const newStudent = new Student(iname, lastName, email, lessonId);
-	studentList.push(newStudent);
-	return newStudent;
+  const newStudent = new Student(iname, lastName, email, lessonId);
+  studentList.push(newStudent);
+  return newStudent;
 }
 
 function connectStudentToRegister(student, lessonId) {
-	const foundedStudent = studentList.find((a) => a.id === student.id);
-	if (foundedStudent) {
-		foundedStudent.lessonId = lessonId;
-		console.log(
-			`Connessione dello studente: ${foundedStudent.iname} riuscita con la lezione ${lessonId}`
-		);
-		return true;
-	} else {
-		console.log(
-			`Connessione dello studente con ID ${student.id} non riuscita con la lezione ${lessonId}`
-		);
-		return false;
-	}
+  const foundedStudent = studentList.find((a) => a.id === student.id);
+  if (foundedStudent) {
+    foundedStudent.lessonId = lessonId;
+    console.log(
+      `Connessione dello studente: ${foundedStudent.iname} riuscita con la lezione ${lessonId}`
+    );
+    return true;
+  } else {
+    console.log(
+      `Connessione dello studente con ID ${student.id} non riuscita con la lezione ${lessonId}`
+    );
+    return false;
+  }
 }
 
 function deleteStudent(studentId) {
-	const studentIndex = studentList.findIndex(
-		(student) => student.id === studentId
-	);
-	if (studentIndex !== -1) {
-		const deletedStudent = studentList.splice(studentIndex, 1)[0];
-		console.log(`Studente ${deletedStudent.name} rimosso con successo.`);
-	} else {
-		console.log(`Studente con ID ${studentId} non trovato.`);
-	}
+  const studentIndex = studentList.findIndex(
+    (student) => student.id === studentId
+  );
+  if (studentIndex !== -1) {
+    const deletedStudent = studentList.splice(studentIndex, 1)[0];
+    console.log(`Studente ${deletedStudent.name} rimosso con successo.`);
+  } else {
+    console.log(`Studente con ID ${studentId} non trovato.`);
+  }
 }
 
 /*function updateStudent(){};
@@ -60,27 +60,27 @@ updateAttendence();*/
 
 //const schoolRecord = new SchoolRecord("school1", "Math");
 const student1 = {
-	id: "1",
-	iname: "Giuseppe",
-	lastName: "Barca",
-	email: "giuseppeb@gmail.com",
-	lessonId: "Math",
+  id: "1",
+  iname: "Giuseppe",
+  lastName: "Barca",
+  email: "giuseppeb@gmail.com",
+  lessonId: "Math",
 };
 const student2 = {
-	id: "2",
-	iname: "fatim",
-	lastName: "cangialosi",
-	email: "fatima@gmail.com",
-	lessonId: "Math",
+  id: "2",
+  iname: "fatim",
+  lastName: "cangialosi",
+  email: "fatima@gmail.com",
+  lessonId: "Math",
 };
 const student3 = {
-	id: "3",
-	iname: "alessandro",
-	lastName: "russo",
-	email: "alerusso@gmail.com",
-	lessonId: "Math",
+  id: "3",
+  iname: "alessandro",
+  lastName: "russo",
+  email: "alerusso@gmail.com",
+  lessonId: "Math",
 };
 
-SchoolRecord.addStudent(student1);
-SchoolRecord.addStudent(student2);
-SchoolRecord.deleteStudent("3");
+//SchoolRecord.addStudent(student1);
+//SchoolRecord.addStudent(student2);
+//SchoolRecord.deleteStudent("3");
