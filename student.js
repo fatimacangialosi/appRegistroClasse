df52915196377c1c4c591789b4e989dfe06622f9
BarcaGiuseppe <gbarca1995@gmail.com>
2024-01-09 17:57:22 +0100

    chore(app): formatting, clean up comments and console.log

--- student.js
+++ student.js
@@ -1,13 +1,7 @@
-//import Student from "./student";
-
 class Student {
   constructor() {
     this.studentList = JSON.parse(localStorage.getItem("AllStudents")) || [];
-    //this.studentList.push(student1);
-    //this.studentList.push(student2);
   }
-  // Lista di tutti gli studenti
-  //studentList = [];
 
   createStudent(iname, lastName, email, phoneNumber) {
     const newStudent = {
@@ -15,7 +9,7 @@ class Student {
         "student" +
         Math.random().toString(16).slice(2) +
         Date.now().toString(16),
-      name: iname, // Modifica questa linea
+      name: iname,
       lastName: lastName,
       email: email,
       phoneNumber: phoneNumber,
@@ -36,7 +30,6 @@ class Student {
   }
 
   updateStudent({ studentId, name, lastName, email, phoneNumber }) {
-    //console.log("student.id: " + student.id);
     console.log("studentId: " + studentId);
     console.log("name: " + lastName);
     const studentIndex = this.studentList.findIndex(
@@ -46,55 +39,18 @@ class Student {
         return student.id === studentId;
       }
     );
-    console.log(studentIndex);
     if (studentIndex != -1) {
       this.studentList[studentIndex].name = name;
       this.studentList[studentIndex].lastName = lastName;
       this.studentList[studentIndex].email = email;
       this.studentList[studentIndex].phoneNumber = phoneNumber;
-      /*name
-        ? (this.studentList[studentIndex].name = name)
-        : (this.studentList[studentIndex].name =
-            this.studentList[studentIndex].name);
-      lastName
-        ? (this.studentList[studentIndex].lastName = lastName)
-        : (this.studentList[studentIndex].lastName =
-            this.studentList[studentIndex].lastName);
-      email
-        ? (this.studentList[studentIndex].email = email)
-        : (this.studentList[studentIndex].email =
-            this.studentList[studentIndex].email);
-      phoneNumber
-        ? (this.studentList[studentIndex].phoneNumber = phoneNumber)
-        : (this.studentList[studentIndex].phoneNumber =
-            this.studentList[studentIndex].phoneNumber);*/
     } else {
       console.log("non fa update student");
     }
-    /*studentIndex.iname = iname;
-      studentIndex.lastName = lastName;
-      studentIndex.email = email;
-      studentIndex.phoneNumber = phoneNumber;*/
     localStorage.setItem("AllStudents", JSON.stringify(this.studentList));
 
     return studentIndex;
   }
-  /*function connectStudentToRegister(student, lessonId) {
-      const foundedStudent = studentList.find((a) => a.id === student.id);
-      if (foundedStudent) {
-        foundedStudent.lessonId = lessonId;
-        console.log(
-          `Connessione dello studente: ${foundedStudent.iname} riuscita con la lezione ${lessonId}`
-        );
-        return true;
-      } else {
-        console.log(
-          `Connessione dello studente con ID ${student.id} non riuscita con la lezione ${lessonId}`
-        );
-        return false;
-      }
-    }*/
-
   deleteStudent(studentId) {
     const studentIndex = this.studentList.findIndex(
       (student) => student.id === studentId
@@ -109,59 +65,4 @@ class Student {
   }
 }
 
-/*function updateStudent(){};
-    function getStudent(
-    );
-    function assignStudentVote();
-    function updateStudentVote();
-    deleteStudentVote();
-    createAttendance();
-    deleteAttendance();
-    updateAttendence();*/
-
-// remainder class Student {
-//constructor(iname, lastName, email, lessonId)
-
-//const schoolRecord = new SchoolRecord("school1", "Math");
-const student1 = {
-  id: "1",
-  name: "Giuseppe",
-  lastName: "Barca",
-  email: "giuseppeb@gmail.com",
-  phoneNumber: "Math",
-};
-const student2 = {
-  id: "2",
-  name: "fatim",
-  lastName: "cangialosi",
-  email: "fatima@gmail.com",
-  phoneNumber: "Math",
-};
-const student3 = {
-  id: "3",
-  iname: "alessandro",
-  lastName: "russo",
-  email: "alerusso@gmail.com",
-  lessonId: "Math",
-};
-
-//SchoolRecord.addStudent(student1);
-//SchoolRecord.addStudent(student2);
-//SchoolRecord.deleteStudent("3");
-
-//createStudent(1, "giuseppe", "barca", "pino@pino.it", "3281'01202");
-//createStudent(2, "giuseppe", "barca", "pino@pino.it", "3281'01202");
-//updateStudent({ studentId: 1, name: "alessandro" });
-//console.log(getStudent(1));
 export default Student;
-
-/*const studentList = new StudentList();
-  studentList.createStudent(1, "giuseppe", "barca", "pino@pino.it", "3281'01202");
-  studentList.createStudent(
-    2,
-    "fatima",
-    "cangialosi",
-    "fatim@pino.it",
-    "3281'01202"
-  );
-  studentList.getStudent(1);*/
